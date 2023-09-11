47f1e683f2b2604d3cdc33f4c330755f594f2687
Ignat09 <lilia.lilia.lilia1984@gmail.com>
2023-09-11 12:57:34 +0300

    commit number two

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
 	<body>
 		<?php
 $temp = rand(350, 420) / 10;
-if ($temp < 38 and $temp > 36) {
-    echo "<h3>", "у вас $temp °, вы здоровы";
-} elseif ($temp > 38 and $temp < 40) {
-    echo "<h3>", "у вас $temp °, вы заболеваете";
-} elseif ($temp > 40 or $temp < 36) {
-    echo "<h3>", "у вас $temp °, вы больны!";
-}
+// if ($temp < 38 and $temp > 36) {
+//     echo "<h3>", "у вас $temp °, вы здоровы", </h3>;
+// } elseif ($temp > 38 and $temp < 40) {
+//     echo "<h3>", "у вас $temp °, вы заболеваете", </h3>;
+// } elseif ($temp > 40 or $temp < 36) {
+//     echo "<h3>", "у вас $temp °, вы больны!", </h3>;
+// }
+$str = ($temp < 37) ? "<h3>" . "у вас $temp °, вы здоровы" . "</h3>" : (($temp > 37 and $temp < 38) ? "<h3>" . "у вас $temp °, вы заболеваете" . "</h3>" : "<h3>" . "у вас $temp °, вы больны!" . "</h3>");
+echo $str;
 ?>
 	</body>
 </html>
